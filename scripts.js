701c7f85ccbc3fa48a4b86290b70200456848e96
Nathan Culley <nathan@nathanculley.com>
2022-12-16 15:37:46 -0500

    Convert newGame into public function referencing private function _newBoard.
    gameBoard now returns newGame as a function which calls the private function _newBoard; the createBoard and createRow functions have been converted into private functions and renamed as well.

--- scripts.js
+++ scripts.js
@@ -1,33 +1,45 @@
 const gameObject = (() => {
-    const newGame = (rows, columns) => {
+    const _newBoard = (rows, columns) => {
         const gameArray= [];
-        return createBoard(gameArray, rows, columns);
-        // return gameArray;
+        return _createBoard(gameArray, rows, columns);
+    };
+
+    function _createBoard(gameArray, rows, columns) {
+        for (let i = 0; i <= rows - 1; i++) {
+            gameArray[i] = _createRow(columns);
+        }
+        return gameArray;
     }
+    function _createRow(columns) {
+        let rowArray = [];
+        for (let i = 0; i <= columns - 1; i++) {
+            rowArray[i] = new Object();
+        }
+        return rowArray;
+    }
+    function _getSquare(row, col) {
+        return "hello";
+    }
+
     return {
-        newGame,
+        newGame: function(rows, columns) {
+            return _newBoard(rows, columns);
+        },
+        getSquare: function(row, col) {
+            return _getSquare(row, col);
+        },
     }
 })();
 
-// gameObject.newGame(3, 3);
+console.log(gameObject.newGame(3, 3));
+console.log(gameObject.getSquare());
 
 // let gameArray = [];
 // let rows = 3;
 // let columns = 3;
 
-function createRow(columns) {
-    let rowArray = [];
-    for (let i = 0; i <= columns - 1; i++) {
-        rowArray[i] = new Object();
-    }
-    return rowArray;
-}
 
-function createBoard(gameArray, rows, columns) {
-    for (let i = 0; i <= rows - 1; i++) {
-        gameArray[i] = createRow(columns);
-    }
-    return gameArray;
-}
+
+
 // createBoard(gameArray, rows);
 // console.log(createBoard(gameArray, rows));
